abe8277c166cfb1e487a9cf78473cad8060d980b
testname <testemail>
2024-12-24 09:39:24 +0300

    исправлены вспомогательные тесты

--- test/mf_test.cpp
+++ test/mf_test.cpp
@@ -1,5 +1,3 @@
-#include <cstdint>
-
 #define _USE_MATH_DEFINES
 #include <cmath>
 
@@ -15,7 +13,7 @@ int main(void) {
     TEST_FLOAT_T csignal[TEST_N * 2];
     TEST_FLOAT_T cmag[TEST_N];
 
-    for(auto i = 0; i < TEST_N; ++i) {
+    for(int i = 0; i < TEST_N; ++i) {
         // signal_t[i] = 2 + std::sin(i * 2 * M_PI * TEST_F / TEST_N);
         // signal_t[i] += std::sin(i * 2 * M_PI * TEST_F1 / TEST_N);
 
@@ -27,7 +25,7 @@ int main(void) {
 
     /* rfft */
     printf("mf rfft(%d):\n", TEST_N);
-    mf::Rfft<TEST_FLOAT_T, unsigned, TEST_N> rfft;
+    mf::Rfft<TEST_FLOAT_T, TEST_N> rfft;
     (void)sizeof(rfft);
 #if TEST_INVERSE
     rfft.inverse(signal_t, signal_f);
@@ -36,7 +34,7 @@ int main(void) {
     // }
     print_hist_horiz(signal_f, TEST_N, 60);
 
-     printf("scipy rfft(%d):\n", TEST_N);
+    printf("scipy rfft(%d):\n", TEST_N);
     TEST_FLOAT_T scipy[TEST_N] = {
         0.6666667,   0.6593825,   0.6378485,  0.6030057,   0.55637693, 0.5,         0.43633902,  0.36817616,
         0.2984905,   0.23032768,  0.16666667, 0.110289805, 0.06366101, 0.028818175, 0.007284149, 0.0,
@@ -46,7 +44,7 @@ int main(void) {
     print_hist_horiz(scipy, TEST_N, 60);
 #else
     rfft.forward(signal_t, signal_f);
-    for(auto i = 0; i < TEST_N / 2; ++i) {
+    for(int i = 0; i < TEST_N / 2; ++i) {
         printf("%g, %g\n", signal_f[2 * i], signal_f[2 * i + 1]);
         mag[i] = std::sqrt(signal_f[2 * i] * signal_f[2 * i] + signal_f[2 * i + 1] * signal_f[2 * i + 1]);
     }
@@ -61,7 +59,7 @@ int main(void) {
     // #else
     //     cfft.forward(csignal);
     // #endif
-    //     for(auto i = 0; i < TEST_N; ++i) {
+    //     for(int i = 0; i < TEST_N; ++i) {
     //         cmag[i] = sqrt(csignal[2 * i] * csignal[2 * i] + csignal[2 * i + 1] * csignal[2 * i + 1]);
     //     }
     //     print_hist_horiz(cmag, TEST_N / 1, 60);
