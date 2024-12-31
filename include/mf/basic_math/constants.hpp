6216a740396015632de9f4ce092ce15d78e14c2d
s38449 <r.bolgov@kmz.su>
2024-12-31 19:24:48 +0300

    добавлена типизация констант

--- include/mf/basic_math/constants.hpp
+++ include/mf/basic_math/constants.hpp
@@ -4,19 +4,38 @@
 #include "mf/types/floating.hpp"
 #include "mf/utils/config.hpp"
 
-#if defined(__GNUC__) && MF_HAS_FLOAT128_TYPE
-// from boost:
-// This is the only way we can avoid
-// warning: non-standard suffix on floating constant [-Wpedantic]
-// when building with -Wall -pedantic.  Neither __extension__
-// nor #pragma dianostic ignored work :(
-#pragma GCC system_header
+#if defined(__cpp_constexpr)
+#define MF_SPECIALIZE_CONST(n, v, t, s) \
+    template<> struct n<t> { \
+        static constexpr t value = MF_DO_CONCAT(v, s); \
+    };
+#else
+#define MF_SPECIALIZE_CONST(n, v, t, s) \
+    template<> struct n<t> { \
+        static const t value; \
+    }; \
+    const t n<t>::value = MF_DO_CONCAT(v, s);
+#endif
+
+#if defined(__cpp_variable_templates)
+#define MF_DECLARE_CONST(n, v) \
+    template<typename T> struct n {}; \
+    template<typename T> constexpr T MF_DO_CONCAT(n, _v) = n<T>::value; \
+    MF_SPECIALIZE_CONST(n, v, float, f) \
+    MF_SPECIALIZE_CONST(n, v, double, l) \
+    MF_SPECIALIZE_CONST(n, v, long double, l)
+#else
+#define MF_DECLARE_CONST(n, v) \
+    template<typename T> struct n {}; \
+    MF_SPECIALIZE_CONST(n, v, float, f) \
+    MF_SPECIALIZE_CONST(n, v, double, l) \
+    MF_SPECIALIZE_CONST(n, v, long double, l)
 #endif
 
 namespace mf {
-static MF_CONST_OR_CONSTEXPR float_max_t PI = MF_FLOAT_MAX_C(3.141592653589793238462643383279502884e+00);
-static MF_CONST_OR_CONSTEXPR float_max_t SQRT2 = MF_FLOAT_MAX_C(1.414213562373095048801688724209698079e+00);
-static MF_CONST_OR_CONSTEXPR float_max_t SQRT1_2 = MF_FLOAT_MAX_C(7.071067811865475244008443621048490392e-01);
+MF_DECLARE_CONST(pi, 3.141592653589793238462643383279502884e+00)
+MF_DECLARE_CONST(sqrt2, 1.414213562373095048801688724209698079e+00)
+MF_DECLARE_CONST(sqrt1_2, 7.071067811865475244008443621048490392e-01)
 } // namespace mf
 
 #endif // HPP_MF_BASIC_MATH_CONSTANTS
