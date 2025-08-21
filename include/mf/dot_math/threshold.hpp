cc9b65caaaf06021dd9c17edd5bdc3c6e4865689
testname <testemail>
2025-08-21 11:27:12 +0300

    новый интерфейс для ограничителя по уровню

--- include/mf/dot_math/threshold.hpp
+++ include/mf/dot_math/threshold.hpp
@@ -37,6 +37,34 @@ limit_by_threshold(DataType (&limited)[Size], const DataType (&threshold)[Size],
         }
     }
 }
+template<typename DataType, size_t Size>
+MF_OPTIMIZE(3) MF_CONSTEXPR_14 void limit_by_threshold(DataType (&result)[Size],
+                                                       const DataType (&limited)[Size],
+                                                       const DataType (&threshold)[Size],
+                                                       DataType limiter = 0) MF_NOEXCEPT {
+    typedef typename fastest_integral_type<Size>::type idx_t;
+
+    MF_CONST_OR_CONSTEXPR idx_t BLK_SIZE = 4;
+    MF_CONST_OR_CONSTEXPR idx_t BLKS = Size / BLK_SIZE;
+    MF_CONST_OR_CONSTEXPR idx_t RESIDUE = Size % BLK_SIZE;
+
+    for(idx_t i = 0; i != BLKS; ++i) {
+        result[BLK_SIZE * i + 0] =
+            limited[BLK_SIZE * i + 0] < threshold[BLK_SIZE * i + 0] ? limiter : limited[BLK_SIZE * i + 0];
+        result[BLK_SIZE * i + 1] =
+            limited[BLK_SIZE * i + 1] < threshold[BLK_SIZE * i + 1] ? limiter : limited[BLK_SIZE * i + 1];
+        result[BLK_SIZE * i + 2] =
+            limited[BLK_SIZE * i + 2] < threshold[BLK_SIZE * i + 2] ? limiter : limited[BLK_SIZE * i + 2];
+        result[BLK_SIZE * i + 3] =
+            limited[BLK_SIZE * i + 3] < threshold[BLK_SIZE * i + 3] ? limiter : limited[BLK_SIZE * i + 3];
+    }
+
+    MF_IF_CONSTEXPR(RESIDUE != 0) {
+        for(idx_t i = BLKS * BLK_SIZE; i != Size; ++i) {
+            result[i] = limited[i] < threshold[i] ? limiter : limited[i];
+        }
+    }
+}
 } // namespace mf
 
 #endif // HPP_MF_DOT_MATH_THRESHOLD
