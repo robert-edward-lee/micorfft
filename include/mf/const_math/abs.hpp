bd15b28a09d324ac01e7ce275fb924f41cfd824c
testname <testemail>
2026-03-03 17:16:50 +0300

    round

--- include/mf/const_math/abs.hpp
+++ include/mf/const_math/abs.hpp
@@ -1,6 +1,8 @@
 #ifndef HPP_MF_CONST_MATH_ABS
 #define HPP_MF_CONST_MATH_ABS
 
+#include "mf/types/integral.hpp"
+
 namespace mf { namespace cm {
 template<typename T> constexpr T abs(T x) noexcept {
     return x >= T(0) ? x : -x;
@@ -10,6 +12,9 @@ template<typename T> constexpr T abs(T x) noexcept {
  */
 template<typename T> constexpr T sgn(T x) noexcept {
     return (T(0) < x) - (x < T(0));
+}
+template<typename T, typename U> constexpr T round(U x) noexcept {
+    return static_cast<int64_t>(x + U(0.5));
 }
 }} /* namespace mf::cm */
 
